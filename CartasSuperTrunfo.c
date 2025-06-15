4d252e98751a7c4cc60d11a096451e4c2c520a88
victorsilvas7 <victor_souza702@hotmail.com>
2025-06-15 20:51:23 +0000

    pib percapita

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo 
+// Desafio Super Trunfo
 
-int main() {
-       char estado1[50], estado2[50], codigocarta1[50],codigocarta2[50],
+int main(){
+
+    char estado1[50], estado2[50], codigocarta1[50],codigocarta2[50],
     cidade1[50],cidade2[50];
 
     int populaçao1, populaçao2, pontosturisticos1, pontosturisticos2, carta1,carta2;
 
-    float area1, area2,pib1, pib2;
+    float area1, area2,pib1, pib2,densidade1, densidade2, pibpercapita1, pibpercapita2;
 
 // Cadastramento da carta1 via usuário
 
@@ -64,6 +65,11 @@ scanf("%f", &pib2);
 printf("Digite o numero de pontos turiscos: ");
 scanf("%d", &pontosturisticos2);
 
+densidade1 = populaçao1 / area1;
+densidade2 = populaçao2 / area2;
+
+pibpercapita1= pib1 / populaçao1;
+pibpercapita2= pib2 / populaçao2;
 
 //Print da carta1 cadastrada
 printf("\n");
@@ -75,6 +81,8 @@ printf("População: %d \n",populaçao1);
 printf("área: %f \n",area1);
 printf("PIB: %f \n",pib1);
 printf("Número de pontos turiscos: %d \n",pontosturisticos1);
+printf("Densidade Populacional: %f \n",densidade1);
+printf("PIB per capita: %f \n",pibpercapita1);
 
 //Print da carta2
 
@@ -87,6 +95,8 @@ printf("População: %d \n",populaçao2);
 printf("área: %f \n",area2);
 printf("PIB: %f \n",pib2);
 printf("Número de pontos turiscos: %d \n",pontosturisticos2);
+printf("Densidade Populacional: %f \n",densidade2);
+printf("PIB per capita: %f \n",pibpercapita2);
 
 return 0;
 
